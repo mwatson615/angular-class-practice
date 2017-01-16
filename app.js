7282891a24aa98ad675fc38af93653e41fc2e0e1
Michael Watson <m.watson.tn@gmail.com>
2017-01-16 13:53:52 -0600

    end in class practice

--- app.js
+++ app.js
@@ -1,3 +1,25 @@
 console.log("app.js")
 
-var app = angular.module('whateverApp')
+var app = angular.module('whateverApp', ['ngRoute'])
+
+app.config(function($routeProvider) {
+	$routeProvider
+		.when('/', {
+			controller: 'MainCtrl',
+			templateUrl: 'partials/main.html'
+		})
+		.when('/list', {
+			controller: 'ListCtrl',
+			templateUrl: 'partials/list.html'
+		})
+})
+
+app.controller('ListCtrl', function($scope) {
+	$scope.myVariable = "angular"
+	console.log("here is the list controller")
+})
+
+app.controller('MainCtrl', function($scope) {
+	$scope.myVariable = "potatoes"
+	$scope.otherVariable = "french fries"
+})
